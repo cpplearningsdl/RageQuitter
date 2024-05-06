854c729dca305bad5f107bf282f369c2e94b2fe8
cpplearningsdl <66233959+cpplearningsdl@users.noreply.github.com>
2024-05-06 16:19:21 -0400

    Update content.js
    removed debug console logging

--- content.js
+++ content.js
@@ -7,13 +7,6 @@ let resignButton;
 // Function to set the button if it's null
 function setButton() {
   resignButton = document.querySelector('[data-cy="resign-button"]');
-  if (!resignButton) {
-    // Code to set the button here, if necessary
-    console.log("Button not set.");
-  } else {
-    console.log("Resign button found. ID:", resignButton.id);
-    console.log("Resign button data:", resignButton.dataset);
-  }
 }
 
 // Periodically check for the button every 5 seconds
@@ -23,6 +16,5 @@ setInterval(setButton, 5000);
 window.addEventListener('beforeunload', function(event) {
   if (resignButton) {
     resignButton.click();
-    console.log("Resign button clicked.");
   }
 });
